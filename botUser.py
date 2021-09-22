ba82f7a0c463de119cad4f31eac8a682e83e6ee8
Stanislav <sfedoruk@mail.ru>
2021-09-22 22:25:21 +0600

    Third commit (Fixed bot)

--- botUser.py
+++ botUser.py
@@ -2,53 +2,52 @@ from random import randint
 import requests
 
 
-def phone(n):
-    start = 10 ** (n - 1)
-    end = (10 ** n) - 1
-    return randint(start, end)
-
-def is_staff():
-    choise = randint(0, 1)
-    if choise == 0:
-        is_staff = False
-    elif choise == 1:
-        is_staff = True
-    return is_staff
-
-def is_active():
-    choise =randint(0,1)
-    if choise == 0:
-        is_active = False
-    elif choise == 1:
-        is_active = True
-    return is_active
-
-
 class UserBot:
-    def __init__(self, phone, is_staff, is_active, password):
-        self.phone = phone
-        self.password = password
-        self.is_active = is_active
-        self.is_staff = is_staff
+    def __init__(self):
+        self.phone = self.number(10)
+        self.password = self.password(6)
+        self.is_active = self.is_active()
+        self.is_staff = self.is_staff()
 
     def getInfo(self):
-        print(f'Phone: {self.phone}, Password: {self.password}')
+        print(f'Phone: {self.phone}, Password: {self.password}, Staff: {self.is_staff}, Activ: {self.is_active}')
 
-    def number(n):
+    def number(self, n):
         start = 10 ** (n - 1)
         end = (10 ** n) - 1
         return randint(start, end)
+
+    def password(self, n):
+        start = 10 ** (n - 1)
+        end = (10 ** n) - 1
+        return randint(start, end)
+
+    def is_staff(self):
+        choise = randint(0, 1)
+        if choise == 0:
+            is_staff = False
+        elif choise == 1:
+            is_staff = True
+        return is_staff
+
+    def is_active(self):
+        choise = randint(0, 1)
+        if choise == 0:
+            is_active = False
+        elif choise == 1:
+            is_active = True
+        return is_active
 
 
 howUsersCreate = int(input('Введите количество юзеров которое нужно создать: '))
 mass = []
 for i in range(howUsersCreate):
-    user = UserBot(phone=phone(10), is_staff=is_staff(), is_active=is_active(), password=phone(6))
+    user = UserBot()
     try:
         r = requests.post('http://127.0.0.1:8000/users/registration/', data=({'phone': user.phone,
                                                                          'password': user.password,
-                                                                              'is_staff': is_staff(),
-                                                                              'is_active()':is_active(),
+                                                                              'is_staff': user.is_staff(),
+                                                                              'is_active()': user.is_active(),
                                                                               'kind': 'User1'
                                                                               }))
         mass.append(user)
