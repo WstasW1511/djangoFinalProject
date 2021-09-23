03c26b32dbcbe7fefec32e92adfb2d794d600dc3
Stanislav <sfedoruk@mail.ru>
2021-09-23 17:53:31 +0600

    6 commit (ver2 bot)

--- botUser.py
+++ botUser.py
@@ -3,27 +3,23 @@ from random import randint, choice
 import requests
 from string import ascii_letters
 from datetime import datetime
-
-
-
+from string import digits
 
 
 class UserBot:
     def __init__(self):
         self.phone = self.number(10)
-        self.password = self.password(6)
+        self.password = self.password(8)
         self.is_active = self.is_active()
         self.is_staff = self.is_staff()
 
     def number(self, n):
-        start = 10 ** (n - 1)
-        end = (10 ** n) - 1
-        return randint(start, end)
+        number = ''.join(choice(digits) for i in range(n))
+        return number
 
     def password(self, n):
-        start = 10 ** (n - 1)
-        end = (10 ** n) - 1
-        return randint(start, end)
+        password = ''.join(choice(digits) for i in range(n))
+        return password
 
     def is_staff(self):
         choise = randint(0, 1)
@@ -44,9 +40,9 @@ class UserBot:
     def getInfo(self):
         print(f'Phone: {self.phone}, Password: {self.password}, Staff: {self.is_staff}, Activ: {self.is_active}')
 
+
 howUsersCreate = int(input('Введите количество юзеров которое нужно создать: '))
 start_time = datetime.now()
-mass = []
 for i in range(howUsersCreate):
     user = UserBot()
     try:
@@ -56,33 +52,22 @@ for i in range(howUsersCreate):
                                                                               'is_active': user.is_active,
                                                                               'kind': 'User1'
                                                                               }))
-        mass.append(user)
-        print('Registration', r, i+1)
-    except:
-        print('dont created')
-
-try:
-    for i in mass:
         log_in = requests.post('http://127.0.0.1:8000/users/login/', data=({
-            'phone': i.phone,
-            'password': i.password
-        }))
-
+                                                                            'phone': user.phone,
+                                                                            'password': user.password
+                                                                            }))
         tokens = log_in.json()
         actoken = tokens['tokens']['access']
         views = ['Blog', 'News', 'Advertising']
         view = random.choice(views)
         text = ''.join(choice(ascii_letters) for i in range(10))
-        a = requests.get('http://127.0.0.1:8000/posts/list/')
-        r = requests.post('http://127.0.0.1:8000/posts/list/', headers={'Authorization': 'Bearer {}'.format(actoken)},
+        post = requests.post('http://127.0.0.1:8000/posts/list/', headers={'Authorization': 'Bearer {}'.format(actoken)},
                                                                 data={
                                                                 'text': text,
                                                                 'view': view
                                                                     })
-
-        print("Create Post: ", r)
-    print('Created', howUsersCreate, 'posts')
-except:
-    print("Don't login!")
+    except:
+        print("Don't login!")
+print('Created', howUsersCreate, 'posts')
 end_time = datetime.now()
 print('Duration: {}'.format(end_time - start_time))
